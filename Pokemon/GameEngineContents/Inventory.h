04741a63bfea26cbf5fb8e2e3f3a0e55e9277e63
이삭 <alwlsdydtjd1@gmail.com>
2022-05-17 14:59:04 +0900

    이삭 : 인벤토리 클래스 추가

---
+++ Pokemon/GameEngineContents/Inventory.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// Ό³Έν :
+class Inventory
+{
+public:
+	// constrcuter destructer
+	Inventory();
+	~Inventory();
+
+	// delete Function
+	Inventory(const Inventory& _Other) = delete;
+	Inventory(Inventory&& _Other) noexcept = delete;
+	Inventory& operator=(const Inventory& _Other) = delete;
+	Inventory& operator=(Inventory&& _Other) noexcept = delete;
+
+protected:
+
+private:
+
+};
+
