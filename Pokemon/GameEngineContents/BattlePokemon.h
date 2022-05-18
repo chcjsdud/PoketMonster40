958c3bca3f12da230067cec6ae666a6bc88f905d
wndgur98@gmail.com <wndgur98@gmail.com>
2022-05-18 17:21:37 +0900

    장중혁 : battle 클래스 추가

---
+++ Pokemon/GameEngineContents/BattlePokemon.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <GameEngine/GameEngineActor.h>
+#include <GameEngine/GameEngineRenderer.h>
+
+// Ό³Έν :
+class BattlePokemon : public GameEngineActor
+{
+public:
+	// constrcuter destructer
+	BattlePokemon();
+	~BattlePokemon();
+
+	// delete Function
+	BattlePokemon(const BattlePokemon& _Other) = delete;
+	BattlePokemon(BattlePokemon&& _Other) noexcept = delete;
+	BattlePokemon& operator=(const BattlePokemon& _Other) = delete;
+	BattlePokemon& operator=(BattlePokemon&& _Other) noexcept = delete;
+
+protected:
+	void Start() override;
+	void Update() override;
+
+private:
+
+	GameEngineRenderer* PlayerPokemon_;
+	GameEngineRenderer* OpponentPokemon_;
+	
+};
+
