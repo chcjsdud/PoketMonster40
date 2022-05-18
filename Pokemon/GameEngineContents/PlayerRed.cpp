7ab6f2e818905081d89087231da497ea5e869bd2
Rotus <12rotus31@gmail.com>
2022-05-18 16:40:17 +0900

    류진아 : 플레이어 상태 애니메이션 추가

--- Pokemon/GameEngineContents/PlayerRed.cpp
+++ Pokemon/GameEngineContents/PlayerRed.cpp
@@ -6,6 +6,8 @@
 #include <GameEngine/GameEngineImageManager.h>
 #include <GameEngine/GameEngineRenderer.h>
 
+PlayerRed* PlayerRed::MainRed_ = nullptr;
+
 PlayerRed::PlayerRed() 
 {
 }
@@ -14,32 +16,137 @@ PlayerRed::~PlayerRed()
 {
 }
 
+void PlayerRed::ChangeState(RedState _State)
+{
+	if (CurrentState_ != _State)
+	{
+		switch (_State)
+		{
+		case RedState::Idle:
+			IdleStart();
+			break;
+		case RedState::Walk:
+			WalkStart();
+			break;
+		case RedState::Run:
+			RunStart();
+			break;
+		case RedState::Max:
+			break;
+		default:
+			break;
+		}
+	}
+	CurrentState_ = _State;
+}
+
+void PlayerRed::StateUpdate()
+{
+	switch (CurrentState_)
+	{
+	case RedState::Idle:
+		IdleUpdate();
+		break;
+	case RedState::Walk:
+		WalkUpdate();
+		break;
+	case RedState::Run:
+		RunUpdate();
+		break;
+	case RedState::Max:
+		break;
+	default:
+		break;
+	}
+}
+
+void PlayerRed::DirAnimationCheck()
+{
+	RedDir CheckDir_ = CurrentDir_;
+
+	if (true == GameEngineInput::GetInst()->IsPress("Up"))
+	{
+		CheckDir_ = RedDir::Up;
+		ChangeDirText_ = "Up";
+	}
+	if (true == GameEngineInput::GetInst()->IsPress("Down"))
+	{
+		CheckDir_ = RedDir::Down;
+		ChangeDirText_ = "Down";
+	}
+	if (true == GameEngineInput::GetInst()->IsPress("Right"))
+	{
+		CheckDir_ = RedDir::Right;
+		ChangeDirText_ = "Right";
+	}
+	if (true == GameEngineInput::GetInst()->IsPress("Left"))
+	{
+		CheckDir_ = RedDir::Left;
+		ChangeDirText_ = "Left";
+	}
+}
+
+
 void PlayerRed::Start()
 {
-	RedRender_ = CreateRenderer("IdleDown.bmp");
+	{
+		GameEngineImage*
+		Image = GameEngineImageManager::GetInst()->Find("IdleUp.bmp");
+		Image->Cut({ 56,76 });
+		Image = GameEngineImageManager::GetInst()->Find("IdleDown.bmp");
+		Image->Cut({ 56,76 });
+		Image = GameEngineImageManager::GetInst()->Find("IdleLeft.bmp");
+		Image->Cut({ 56,76 });
+		Image = GameEngineImageManager::GetInst()->Find("IdleRight.bmp");
+		Image->Cut({ 56,76 });
+
+		Image = GameEngineImageManager::GetInst()->Find("WalkUp.bmp");
+		Image->CutCount(3, 1);
+		Image = GameEngineImageManager::GetInst()->Find("WalkDown.bmp");
+		Image->CutCount(3, 1);
+		Image = GameEngineImageManager::GetInst()->Find("WalkLeft.bmp");
+		Image->CutCount(3, 1);
+		Image = GameEngineImageManager::GetInst()->Find("WalkRight.bmp");
+		Image->CutCount(3, 1);
+
+		Image = GameEngineImageManager::GetInst()->Find("RunUp.bmp");
+		Image->CutCount(3, 1);
+		Image = GameEngineImageManager::GetInst()->Find("RunDown.bmp");
+		Image->CutCount(3, 1);
+		Image = GameEngineImageManager::GetInst()->Find("RunLeft.bmp");
+		Image->CutCount(3, 1);
+		Image = GameEngineImageManager::GetInst()->Find("RunRight.bmp");
+		Image->CutCount(3, 1);
+	}
+
+	RedRender_ = CreateRenderer();
+	RedRender_->CreateAnimation("IdleUp.bmp", "IdleUp", 0, 0, 0.0f, false);
+	RedRender_->CreateAnimation("IdleDown.bmp", "IdleDown", 0, 0, 0.0f, false);
+	RedRender_->CreateAnimation("IdleLeft.bmp", "IdleLeft", 0, 0, 0.0f, false);
+	RedRender_->CreateAnimation("IdleRight.bmp", "IdleRight", 0, 0, 0.0f, false);
+	
+	RedRender_->CreateAnimation("WalkUp.bmp", "WalkUp", 0, 2, 0.1f, true);
+	RedRender_->CreateAnimation("WalkDown.bmp", "WalkDown", 0, 2, 0.1f, true);
+	RedRender_->CreateAnimation("WalkLeft.bmp", "WalkLeft", 0, 2, 0.1f, true);
+	RedRender_->CreateAnimation("WalkRight.bmp", "WalkRight", 0, 2, 0.1f, true);
+
+	RedRender_->CreateAnimation("RunUp.bmp", "RunUp", 0, 2, 0.1f, true);
+	RedRender_->CreateAnimation("RunDown.bmp", "RunDown", 0, 2, 0.1f, true);
+	RedRender_->CreateAnimation("RunLeft.bmp", "RunLeft", 0, 2, 0.1f, true);
+	RedRender_->CreateAnimation("RunRight.bmp", "RunRight", 0, 2, 0.1f, true);
+
+	RedRender_->ChangeAnimation("IdleDown");
+
+	AnimationName_ = "Idle";
+	CurrentDir_ = RedDir::Down;
+	CurrentState_ = RedState::Idle;
 }
 
 void PlayerRed::Update()
 {
-	float4 MoveDir_ = float4::ZERO;
-	if (true == GameEngineInput::GetInst()->IsPress("Up"))
-	{
-		MoveDir_ = float4::UP;
-	}
-	if (true == GameEngineInput::GetInst()->IsPress("Down"))
-	{
-		MoveDir_ = float4::DOWN;
-	}
-	if (true == GameEngineInput::GetInst()->IsPress("Left"))
-	{
-		MoveDir_ = float4::LEFT;
-	}
-	if (true == GameEngineInput::GetInst()->IsPress("Right"))
-	{
-		MoveDir_ = float4::RIGHT;
-	}
-
-	SetMove(MoveDir_ * GameEngineTime::GetDeltaTime() * 500);
+	DirAnimationCheck();
+	StateUpdate();
+	
 	Camera();
 }
 
@@ -84,4 +191,16 @@ void PlayerRed::Camera()
 		CurrentCameraPos.y = GetLevel()->GetCameraPos().y - (GetLevel()->GetCameraPos().y + CameraRectY - WorldMapScaleY);
 		GetLevel()->SetCameraPos(CurrentCameraPos);
 	}
+}
+
+bool PlayerRed::IsMoveKey()
+{
+	if (false == GameEngineInput::GetInst()->IsPress("Up") &&
+		false == GameEngineInput::GetInst()->IsPress("Down") &&
+		false == GameEngineInput::GetInst()->IsPress("Left") &&
+		false == GameEngineInput::GetInst()->IsPress("Right"))
+	{
+		return false;
+	}
+	return true;
 }
--- Pokemon/GameEngineContents/PlayerRed.h
+++ Pokemon/GameEngineContents/PlayerRed.h
@@ -1,6 +1,21 @@
 #pragma once
 #include <GameEngine/GameEngineActor.h>
 
+enum class RedState
+{
+	Idle,
+	Walk,
+	Run,
+
+	Max
+};
+enum class RedDir
+{
+	Up,
+	Down,
+	Left,
+	Right,
+};
 // Ό³Έν :
 class PlayerRed : public GameEngineActor
 {
@@ -15,17 +30,37 @@ public:
 	PlayerRed& operator=(const PlayerRed& _Other) = delete;
 	PlayerRed& operator=(PlayerRed&& _Other) noexcept = delete;
 
+	static PlayerRed* MainRed_;
+
+	void ChangeState(RedState _State);
+	void StateUpdate();
+	void DirAnimationCheck();
+
 protected:
 
 private:
-	float4 CameraPos_;
+	float4			CameraPos_;
+	RedDir			CurrentDir_;
+	std::string		AnimationName_;
+	std::string		ChangeDirText_;
 
 	GameEngineRenderer* RedRender_;
-	
+	RedState CurrentState_;
+
 	void Start() override;
 	void Update() override;
 	void Render() override;
 
 	void Camera();
+
+	bool IsMoveKey();
+
+	void IdleUpdate();
+	void WalkUpdate();
+	void RunUpdate();
+
+	void IdleStart();
+	void WalkStart();
+	void RunStart();
 };
 
