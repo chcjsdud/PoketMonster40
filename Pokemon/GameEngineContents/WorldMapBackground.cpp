f25bdbd723cf80b0d65680b42d5eab2a17ffb9f7
장병문 <jundead7981@gmail.com>
2022-05-18 15:28:12 +0900

    장병문 : 룸 리소스 위치 변경 및 배경 위치 수정

--- Pokemon/GameEngineContents/WorldMapBackground.cpp
+++ Pokemon/GameEngineContents/WorldMapBackground.cpp
@@ -1,7 +1,12 @@
 #include "WorldMapBackground.h"
 #include <GameEngine/GameEngineRenderer.h>
+#include <GameEngineBase/GameEngineInput.h>
+#include <GameEngineBase/GameEngineWindow.h>
 
 WorldMapBackground::WorldMapBackground() 
+	: WorldMap1_(nullptr)
+	, RoomsRenderer_(nullptr)
+	, TestRenderer_(nullptr)
 {
 }
 
@@ -13,6 +18,44 @@ void WorldMapBackground::Start()
 {
 	WorldMap1_ = CreateRenderer("test3.bmp");
 	WorldMap1_->SetPivot({1000, -1800});
-	CreateRenderer("test2.bmp");
+	RoomsRenderer_ = CreateRenderer("Room.bmp");
+	RoomsRenderer_->SetPivot({-2500, 0});
+
+	//TestRenderer_ = CreateRenderer("White.bmp");
+	//TestRenderer_->SetPivot({ -3250, -350 });
+
+	//CreateRenderer("White.bmp")->SetPivot(GameEngineWindow::GetScale().Half());
+
+	AllPosVector.push_back({ -1420, -100 });
+	AllPosVector.push_back({ -1150, -350 });
+	AllPosVector.push_back({ -3250, -350 });
 }
 
+void WorldMapBackground::Update()
+{
+	/////////////////// 테스트용 맵 이동
+	//if (true == GameEngineInput::GetInst()->IsPress("Up"))
+	//{
+	//	WorldMap1_->SetPivot(WorldMap1_->GetPivot() + float4::DOWN * 3.0f);
+	//	RoomsRenderer_->SetPivot(RoomsRenderer_->GetPivot() + float4::DOWN * 3.0f);
+	//	TestRenderer_->SetPivot(TestRenderer_->GetPivot() + float4::DOWN * 3.0f);
+	//}
+	//if (true == GameEngineInput::GetInst()->IsPress("Down"))
+	//{
+	//	WorldMap1_->SetPivot(WorldMap1_->GetPivot() + float4::UP * 3.0f);
+	//	RoomsRenderer_->SetPivot(RoomsRenderer_->GetPivot() + float4::UP * 3.0f);
+	//	TestRenderer_->SetPivot(TestRenderer_->GetPivot() + float4::UP * 3.0f);
+	//}
+	//if (true == GameEngineInput::GetInst()->IsPress("Left"))
+	//{
+	//	WorldMap1_->SetPivot(WorldMap1_->GetPivot() + float4::RIGHT * 3.0f);
+	//	RoomsRenderer_->SetPivot(RoomsRenderer_->GetPivot() + float4::RIGHT * 3.0f);
+	//	TestRenderer_->SetPivot(TestRenderer_->GetPivot() + float4::RIGHT * 3.0f);
+	//}
+	//if (true == GameEngineInput::GetInst()->IsPress("Right"))
+	//{
+	//	WorldMap1_->SetPivot(WorldMap1_->GetPivot() + float4::LEFT * 3.0f);
+	//	RoomsRenderer_->SetPivot(RoomsRenderer_->GetPivot() + float4::LEFT * 3.0f);
+	//	TestRenderer_->SetPivot(TestRenderer_->GetPivot() + float4::LEFT * 3.0f);
+	//}
+}
--- Pokemon/GameEngineContents/WorldMapBackground.h
+++ Pokemon/GameEngineContents/WorldMapBackground.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <GameEngine/GameEngineActor.h>
+#include <vector>
 
 // 설명 :
 class WorldMapBackground : public GameEngineActor
@@ -18,8 +19,15 @@ public:
 protected:
 
 private:
-	void Start() override;
-
 	GameEngineRenderer* WorldMap1_;
+	GameEngineRenderer* RoomsRenderer_;
+	GameEngineRenderer* TestRenderer_;
+	std::vector<float4> AllPosVector;
+	// 0 == 시작지점
+	// 1 == 시작지점 올라왔을때 위치
+	// 2 == 시작지점 내려갔을때 위치
+
+	void Start() override;
+	void Update() override;
 };
 
