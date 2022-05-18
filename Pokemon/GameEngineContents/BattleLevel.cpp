c57364cdbd30e367339b22628ed77641b81ae959
잼순 김 <highyeana@naver.com>
2022-05-18 15:49:38 +0900

    김예나 : 배틀 인터페이스 방향키 조절 테스트

--- Pokemon/GameEngineContents/BattleLevel.cpp
+++ Pokemon/GameEngineContents/BattleLevel.cpp
@@ -1,6 +1,9 @@
+#include <GameEngineBase/GameEngineInput.h>
+
 #include "BattleLevel.h"
 #include "BattleBackground.h"
 #include "BattleInerface.h"
+
 
 BattleLevel::BattleLevel()
 {
@@ -14,14 +17,25 @@ BattleLevel::~BattleLevel()
 
 void BattleLevel::Loading()
 {
+	GameEngineInput::GetInst()->CreateKey("SLeft", VK_LEFT);
+	GameEngineInput::GetInst()->CreateKey("SRight", VK_RIGHT);
+	GameEngineInput::GetInst()->CreateKey("SDown", VK_DOWN);
+	
+	
 	CreateActor<BattleBackground>();
 	
-
 	BattleInerface* Interface = CreateActor<BattleInerface>(3);
 	Interface->SetPosition({ 720.0f, 548.0f });
+
+	Select = CreateActor<BattleSelect>(4);
+	Select->SetPosition({ 530.0f,522.0f }); //기본 Fight위치 셋팅
 }
 
 void BattleLevel::Update()
 {
+	if (true == GameEngineInput::GetInst()->IsDown("SDown"))
+	{
+		Select->SetPosition({ 530.0f,585.0f });
+	}
 }
 
--- Pokemon/GameEngineContents/BattleSelect.h
+++ Pokemon/GameEngineContents/BattleSelect.h
@@ -4,40 +4,36 @@
 //선생님은 생략된 것들도 명시적으로 칠 것이다
 //직접 만들지 않아도 자동으로 생략되어 생성되 있는것들
 
-//설명 : 전투 선택지를 띄우는 인터페이스
+//설명 : 전투를 고르는 화살표
 class GameEngineImage;
-class BattleInerface : public GameEngineActor
+class BattleSelect : public GameEngineActor
 {
 public:
 	//디폴트 생성자
-	BattleInerface();
+	BattleSelect();
 	//디폴트 소멸자
-	~BattleInerface();
+	~BattleSelect();
+
+	
 	
 	
 	//======아래것들은 명시적으로 안쓰겠습니다(delete)======
 	
 	//디폴트 복사 생성자
-	BattleInerface(const BattleInerface& _Other) = delete;
+	BattleSelect(const BattleSelect& _Other) = delete;
 	//RValue Reference 생성자 (나중에 배울것)
-	BattleInerface(BattleInerface&& _Other) noexcept = delete;
+	BattleSelect(BattleSelect&& _Other) noexcept = delete;
 	//operater= (자기자신을 리턴하는)
-	BattleInerface& operator=(const BattleInerface& _Other) = delete;
-	BattleInerface& operator=(BattleInerface&& _Other) noexcept = delete;
-
+	BattleSelect& operator=(const BattleSelect& _Other) = delete;
+	BattleSelect& operator=(BattleSelect&& _Other) noexcept = delete;
 
 
 protected:
-	GameEngineRenderer* InterfaceImage;
+	GameEngineRenderer* SelectKey;
 
 private:
-
-	BattleInerface* MainInterface;
-
-public:
 	void Start() override;
 	void Render() override;
 	void Update() override;
-
 };
 
