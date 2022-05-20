dbbde5f3f18da1da7fd4fffaad649a71ca99272b
Rotus <12rotus31@gmail.com>
2022-05-20 19:30:54 +0900

    류진아 : NPC 띄우기 test 2차

---
+++ Pokemon/GameEngineContents/NPC4.cpp
@@ -0,0 +1,54 @@
+#include "NPC4.h"
+#include <GameEngine/GameEngineImage.h>
+#include <GameEngine/GameEngineImageManager.h>
+#include <GameEngine/GameEngineRenderer.h>
+
+#include "RoomTileMap1.h"
+
+NPC4::NPC4() 
+{
+}
+
+NPC4::~NPC4() 
+{
+}
+
+void NPC4::Start()
+{
+	{
+		GameEngineImage*
+		// Idle
+		Image = GameEngineImageManager::GetInst()->Find("NPC4_.bmp");
+		Image->Cut({ 128,128 });
+
+		// Walk
+		Image = GameEngineImageManager::GetInst()->Find("NPC4_walk_up.bmp");
+		Image->CutCount(3, 1);
+		Image = GameEngineImageManager::GetInst()->Find("NPC4_walk_down.bmp");
+		Image->CutCount(3, 1);
+		Image = GameEngineImageManager::GetInst()->Find("NPC4_walk_right.bmp");
+		Image->CutCount(3, 1);
+		Image = GameEngineImageManager::GetInst()->Find("NPC4_walk_left.bmp");
+		Image->CutCount(3, 1);
+	}
+
+	NPCRender_ = CreateRenderer();
+	NPCRender_->CreateAnimation("NPC4_.bmp", "IdleDown", 0, 0, 0.0f, false);
+	NPCRender_->CreateAnimation("NPC4_.bmp", "IdleLeft", 1, 1, 0.0f, false);
+	NPCRender_->CreateAnimation("NPC4_.bmp", "IdleRight", 2, 2, 0.0f, false);
+	NPCRender_->CreateAnimation("NPC4_.bmp", "IdleUp", 3, 3, 0.0f, false);
+	
+	NPCRender_->CreateAnimation("NPC4_walk_up.bmp", "WalkUp", 0, 2, 0.1f, true);
+	NPCRender_->CreateAnimation("NPC4_walk_down.bmp", "WalkDown", 0, 2, 0.1f, true);
+	NPCRender_->CreateAnimation("NPC4_walk_right.bmp", "WalkRight", 0, 2, 0.1f, true);
+	NPCRender_->CreateAnimation("NPC4_walk_left.bmp", "WalkLeft", 0, 2, 0.1f, true);
+
+	NPCRender_->ChangeAnimation("IdleDown");
+	SetPosition(RoomTileMap1::GetInst()->GetWorldPostion(8, 4));
+}
+
+void NPC4::Update()
+{
+
+}
+
---
+++ Pokemon/GameEngineContents/NPC4.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <GameEngine/GameEngineActor.h>
+
+// Ό³Έν :
+class NPC4 : public GameEngineActor
+{
+public:
+	// constrcuter destructer
+	NPC4();
+	~NPC4();
+
+	// delete Function
+	NPC4(const NPC4& _Other) = delete;
+	NPC4(NPC4&& _Other) noexcept = delete;
+	NPC4& operator=(const NPC4& _Other) = delete;
+	NPC4& operator=(NPC4&& _Other) noexcept = delete;
+
+protected:
+
+private:
+	GameEngineRenderer* NPCRender_;
+	
+	void Start() override;
+	void Update() override;
+};
+
