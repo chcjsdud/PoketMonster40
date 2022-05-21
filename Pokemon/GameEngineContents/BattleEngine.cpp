21f5c72647b8de91d83824a7b23a08475345916e
장중혁 <wndgur98@gmail.com>
2022-05-21 21:59:48 +0900

    장중혁 : BattleLevel 함수 제작중

--- Pokemon/GameEngineContents/BattleEngine.cpp
+++ Pokemon/GameEngineContents/BattleEngine.cpp
@@ -1,7 +1,7 @@
 #include "BattleEngine.h"
 #include "BattleLevel.h"
 #include "PokemonInfoManager.h"
-#include <GameEngineBase/GameEngineDebug.h>
+
 
 
 BattleEngine::BattleEngine()
@@ -14,6 +14,8 @@ BattleEngine::BattleEngine()
 	, BattleStart_(false)
 	, BattleFirstSupporter_(nullptr)
 	, BattleSecondSupporter_(nullptr)
+	, PlayerPokemonState_(nullptr)
+	, OpponentPokemonState_(nullptr)
 {
 }
 
@@ -60,7 +62,6 @@ void BattleEngine::ScanBattleLevel()
 
 void BattleEngine::BattlePage(const std::string& _PlayerSkill, const std::string& _OpponentSkill)
 {
-	CurrentTurn_ = BattleTurn::Wait;
 	//InitialReSetting();
 	{
 		PokemonSkill* PlayerSkill = PokemonInfoManager::GetInst().FindSkill("_PlayerSkill");
@@ -79,22 +80,30 @@ void BattleEngine::BattlePage(const std::string& _PlayerSkill, const std::string
 		if (PlayerPokemon_->GetInfo()->GetSpeed() >= OpponentPokemon_->GetInfo()->GetSpeed())
 		{
 			PlayerTurn_ = true;
-			BattleFirstSupporter_ = new BattlePageSupport(PlayerPokemon_, OpponentPokemon_, PlayerSkill);
-			BattleSecondSupporter_ = new BattlePageSupport(OpponentPokemon_, PlayerPokemon_, OpponentSkill);
+			BattleFirstSupporter_ = new BattlePageSupport(PlayerPokemonState_, OpponentPokemonState_, PlayerSkill);
+			BattleSecondSupporter_ = new BattlePageSupport(OpponentPokemonState_, PlayerPokemonState_, OpponentSkill);
 		}
 		else
 		{
 			PlayerTurn_ = false;
-			BattleFirstSupporter_ = new BattlePageSupport(OpponentPokemon_, PlayerPokemon_, OpponentSkill);
-			BattleSecondSupporter_ = new BattlePageSupport(PlayerPokemon_, OpponentPokemon_, PlayerSkill);
+			BattleFirstSupporter_ = new BattlePageSupport(OpponentPokemonState_, PlayerPokemonState_, OpponentSkill);
+			BattleSecondSupporter_ = new BattlePageSupport(PlayerPokemonState_, OpponentPokemonState_, PlayerSkill);
 		}
 	}
+	CurrentTurn_ = BattleTurn::FirstTurn;
+}
 
+PokemonBattleState* BattleEngine::CreatePokemon(Pokemon* _Pokemon)
+{
+	PokemonBattleState* NewPokemonState = new PokemonBattleState(_Pokemon);
+	AllPokemonState_.push_back(NewPokemonState);
+	return NewPokemonState;
 }
 
 void BattleEngine::LevelChangeStart(GameEngineLevel* _PrevLevel)
 {
-	
+	PlayerPokemonState_ = CreatePokemon(PlayerPokemon_);
+	OpponentPokemonState_ = CreatePokemon(OpponentPokemon_);
 }
 
 void BattleEngine::LevelChangeEnd(GameEngineLevel* _NextLevel)
@@ -110,11 +119,189 @@ void BattleEngine::LevelChangeEnd(GameEngineLevel* _NextLevel)
 		delete BattleSecondSupporter_;
 		BattleSecondSupporter_ = nullptr;
 	}
+
+	if (PlayerPokemonState_ != nullptr)
+	{
+		delete PlayerPokemonState_;
+		PlayerPokemonState_ = nullptr;
+	}
+
+	if (OpponentPokemonState_ != nullptr)
+	{
+		delete OpponentPokemonState_;
+		OpponentPokemonState_ = nullptr;
+	}
+	OpponentPokemon_ = nullptr;
+	PlayerPokemon_ = nullptr;
 }
 
-BattlePageSupport::BattlePageSupport(Pokemon* _AttackPokemon, Pokemon* _DefensePokemon, PokemonSkill* _Skill)
+PokemonBattleState::PokemonBattleState(Pokemon* _Pokemon)
+	: Pokemon_(_Pokemon)
+	, CanAction_(true)
 {
+	{
+		CurrentRank_[PokemonAbility::Att] = 0;
+		CurrentRank_[PokemonAbility::Def] = 0;
+		CurrentRank_[PokemonAbility::SpAtt] = 0;
+		CurrentRank_[PokemonAbility::SpDef] = 0;
+		CurrentRank_[PokemonAbility::Speed] = 0;
+		CurrentRank_[PokemonAbility::Accuracy] = 0;
+		CurrentRank_[PokemonAbility::Evasion] = 0;
+	}
+}
 
+bool PokemonBattleState::SetSkill(Pokemon* _AlppyPokemon, PokemonSkill* _Skill)
+{
+	// 면역일시 return false
+	ApplySkill_[1][_AlppyPokemon].push_back(_Skill);
+	return true;
+}
+float PokemonBattleState::GetRank(const PokemonAbility& _State)
+{
+	switch (_State)
+	{
+	case PokemonAbility::Att:
+	case PokemonAbility::Def:
+	case PokemonAbility::SpAtt:
+	case PokemonAbility::SpDef:
+	case PokemonAbility::Speed:
+		switch (CurrentRank_[_State])
+		{
+		case -6:
+			return 0.25f;
+			break;
+		case -5:
+			return 0.29f;
+			break;
+		case -4:
+			return 0.33f;
+			break;
+		case -3:
+			return 0.40f;
+			break;
+		case -2:
+			return 0.50f;
+			break;
+		case -1:
+			return 0.66f;
+			break;
+		case 0:
+			return 0;
+			break;
+		case 1:
+			return 1.5f;
+			break;
+		case 2:
+			return 2.0f;
+			break;
+		case 3:
+			return 2.5f;
+			break;
+		case 4:
+			return 3.0f;
+			break;
+		case 5:
+			return 3.5f;
+			break;
+		case 6:
+			return 4.0f;
+			break;
+		default:
+			MsgBoxAssert("랭크 수치가 잘못 되었습니다")
+				break;
+		}
+		break;
+	case PokemonAbility::Accuracy:
+	case PokemonAbility::Evasion:
+		switch (CurrentRank_[_State])
+		{
+		case -6:
+			return 0.33f;
+			break;
+		case -5:
+			return 0.38f;
+			break;
+		case -4:
+			return 0.43f;
+			break;
+		case -3:
+			return 0.50f;
+			break;
+		case -2:
+			return 0.60f;
+			break;
+		case -1:
+			return 0.75f;
+			break;
+		case 0:
+			return 0;
+			break;
+		case 1:
+			return 1.33f;
+			break;
+		case 2:
+			return 1.66f;
+			break;
+		case 3:
+			return 2.0f;
+			break;
+		case 4:
+			return 2.33f;
+			break;
+		case 5:
+			return 2.66f;
+			break;
+		case 6:
+			return 3.0f;
+			break;
+		default:
+			MsgBoxAssert("랭크 수치가 잘못 되었습니다")
+				break;
+		}
+	default:
+		break;
+
+	}
+	return 100.0f;
+}
+
+void PokemonBattleState::Update()
+{
+	for (auto& Skill : ApplySkill_)
+	{
+		if (Skill.first == 0)
+		{
+			// Skill.second.second->End()
+		}
+		else if (Skill.first <= -1)
+		{
+			// 무한 지속
+		}
+		else
+		{
+
+		}
+	}
+}
+
+BattlePageSupport::BattlePageSupport(PokemonBattleState* _AttackPokemon, PokemonBattleState* _DefensePokemon, PokemonSkill* _Skill)
+	: AttackPokemon_(_AttackPokemon)
+	, DefensePokemon_(_DefensePokemon)
+	, AttackPokemonSkill_(_Skill)
+	, FinalValue_(0)
+	, Random_(nullptr)
+	, DamgeType_(TypeDamge::Error)
+{
+	Random_ = new GameEngineRandom();
+}
+
+BattlePageSupport::~BattlePageSupport()
+{
+	if (Random_ != nullptr)
+	{
+		delete Random_;
+		Random_ = nullptr;
+	}
 }
 
 TypeDamge BattlePageSupport::ComparePokemonType(const PokemonType& _Attack, const PokemonType& _Defend)
@@ -478,48 +665,48 @@ TypeDamge BattlePageSupport::ComparePokemonType(const PokemonType& _Attack, cons
 
 void BattlePageSupport::UseSkillTurn()
 {
-	PokemonInfo* AttackInfo = AttackPokemon_->GetInfo();
-	PokemonInfo* DefendInfo = DefensePokemon_->GetInfo();
-	switch (AttackPokemonSkill_->GetSkillType())
-	{
-	case SkillType::Physical:
-	{
-		float CorrectionTypeValue = 1.0f;
-		DamgeType_ = ComparePokemonType(AttackPokemonSkill_->GetType() , DefendInfo->GetMyType());
-		switch (DamgeType_)
-		{
-		case TypeDamge::Great:
-			CorrectionTypeValue = 2.0;
-			break;
-		case TypeDamge::Nomal:
-			CorrectionTypeValue = 1.0;
-			break;
-		case TypeDamge::Bad:
-			CorrectionTypeValue = 0.5;
-			break;
-		case TypeDamge::Nothing:
-			CorrectionTypeValue = 0.0;
-			break;
-		default:
-			break;
-		}
-		int AttckValue = AttackInfo->GetAtt();
-		int AttckLevel = AttackInfo->GetMyLevel();
-		int DefendDef = DefendInfo->GetDef();
-		//float SameType = AttackInfo->GetMyType() == _Skill->GetType() ? 1.5f : 1.0f;
-		int RandomValue = (Random_->RandomInt(217, 255)) * 100 / 255;
-		//int FinalDamge = static_cast<int>((static_cast<float>(_Skill->GetValue() * AttckValue * (AttckLevel * 2 / 5 + 2)) * SameType) * CorrectionTypeValue) * RandomValue;
-	}
-		break;
-	case SkillType::Special:
+	//PokemonInfo* AttackInfo;// = AttackPokemon_->GetInfo();
+	//PokemonInfo* DefendInfo; //= DefensePokemon_->GetInfo();
+	//switch (AttackPokemonSkill_->GetSkillType())
+	//{
+	//case SkillType::Physical:
+	//{
+	//	float CorrectionTypeValue = 1.0f;
+	//	DamgeType_ = ComparePokemonType(AttackPokemonSkill_->GetType() , DefendInfo->GetMyType());
+	//	switch (DamgeType_)
+	//	{
+	//	case TypeDamge::Great:
+	//		CorrectionTypeValue = 2.0;
+	//		break;
+	//	case TypeDamge::Nomal:
+	//		CorrectionTypeValue = 1.0;
+	//		break;
+	//	case TypeDamge::Bad:
+	//		CorrectionTypeValue = 0.5;
+	//		break;
+	//	case TypeDamge::Nothing:
+	//		CorrectionTypeValue = 0.0;
+	//		break;
+	//	default:
+	//		break;
+	//	}
+	//	//int AttckValue = AttackInfo->GetAtt();
+	//	//int AttckLevel = AttackInfo->GetMyLevel();
+	//	//int DefendDef; //= DefendInfo->GetDef();
+	//	//float SameType = AttackInfo->GetMyType() == _Skill->GetType() ? 1.5f : 1.0f;
+	//	//int RandomValue = (Random_->RandomInt(217, 255)) * 100 / 255;
+	//	//int FinalDamge = static_cast<int>((static_cast<float>(_Skill->GetValue() * AttckValue * (AttckLevel * 2 / 5 + 2)) * SameType) * CorrectionTypeValue) * RandomValue;
+	//}
+	//	break;
+	//case SkillType::Special:
 
-		break;
-	case SkillType::Status:
+	//	break;
+	//case SkillType::Status:
 
-		break;
-	default:
-		break;
-	}
+	//	break;
+	//default:
+	//	break;
+	//}
 }
 
 void BattleEngine::InitialReSetting()
@@ -527,8 +714,3 @@ void BattleEngine::InitialReSetting()
 	PlayerTurn_ = true;
 }
 
-
-BattlePageSupport::~BattlePageSupport()
-{
-
-}
--- Pokemon/GameEngineContents/BattleEngine.h
+++ Pokemon/GameEngineContents/BattleEngine.h
@@ -1,18 +1,23 @@
 #pragma once
+#include <GameEngineBase/GameEngineRandom.h>
+#include <GameEngineBase/GameEngineDebug.h>
 #include <GameEngine/GameEngineActor.h>
 #include "PokemonSkill.h"
 #include "ContentEnum.h"
 #include "Pokemon.h"
 #include "PokemonInfoManager.h"
 #include "ContentEnum.h"
-#include <GameEngineBase/GameEngineRandom.h>
 
 // 설명 :
 class BattleLevel;
 class BattlePageSupport;
 class PokemonBattleState;
+class ApplySkill;
 enum class TypeDamge;
 enum class BattleTurn;
+
+enum class PokemonAbility;
+
 class BattleEngine : public GameEngineActor
 {
 public:
@@ -40,13 +45,19 @@ protected:
 	void LevelChangeEnd(GameEngineLevel* _NextLevel) override;
 
 private:
+	PokemonBattleState* CreatePokemon(Pokemon* _Pokemon);
+
 	BattleLevel* BattleLevel_;
 	GameEngineRandom* Random_;
 	
 	Pokemon* PlayerPokemon_;
 	Pokemon* OpponentPokemon_;
+
+	PokemonBattleState* PlayerPokemonState_;
+	PokemonBattleState* OpponentPokemonState_;
 	void InitialReSetting();
 
+	std::list<PokemonBattleState*> AllPokemonState_;
 
 // Battle관련
 private:
@@ -56,16 +67,87 @@ private:
 	bool BattleStart_;
 	bool PlayerTurn_;
 };
+
+
 class PokemonBattleState
 {
+	PokemonBattleState();
 public:
-	
+	PokemonBattleState(Pokemon* _Pokemon);
+	~PokemonBattleState()
+	{
+
+	}
+
+	int SetRank(const PokemonAbility& _State, int _Value)
+	{
+		CurrentRank_[_State] += _Value;
+		if (CurrentRank_[_State] > 6)
+		{
+			return CurrentRank_[_State] = 6;
+		}
+		else if (CurrentRank_[_State] < (-6))
+		{
+			return CurrentRank_[_State] = -6;
+		}
+		else
+		{
+			return CurrentRank_[_State];
+		}
+	}
+
+	float GetRank(const PokemonAbility& _State);
+
+
+	// 몇 턴 동안 지속인지 확인하는 함수 필요
+	bool SetSkill(Pokemon* _AlppyPokemon, PokemonSkill* _Skill);
+	void Update();
+
+private:
+	const Pokemon* Pokemon_;
+	bool CanAction_;
+	std::map<PokemonAbility, int> CurrentRank_;
+	// PokemonSkill 상속에 virtual 함수 Ing, End 필요
+
+	std::list<ApplySkill*> AllCurrentApplySkill_;
+	// 현재 적용받는 스킬들
+	class ApplySkill
+	{
+	private:
+		ApplySkill();
+	public :
+		ApplySkill(PokemonBattleState* _CastPokemon, PokemonSkill* _Skill)
+			: CastPokemon_(_CastPokemon)
+			, Skill_(_Skill)
+			, LeftTurn_(0)
+		{
+			// 턴수 필요
+			LeftTurn_ = 1; // 임의의 값
+		}
+		~ApplySkill() {}
+
+		inline void TurnPass()
+		{
+			LeftTurn_ -= 1;
+		}
+
+		void Update() const
+		{
+			//Skill_->;
+		}
+	private :
+		const PokemonBattleState* CastPokemon_;
+		const PokemonSkill* Skill_;
+		int LeftTurn_;
+	};
+	std::map<int, std::map<Pokemon*, std::list<PokemonSkill*>>> ApplySkill_;
+
 };
 
 class BattlePageSupport
 {
 public:
-	BattlePageSupport(Pokemon* _AttackPokemon, Pokemon* _DefensePokemon, PokemonSkill* _Skill);
+	BattlePageSupport(PokemonBattleState* _AttackPokemon, PokemonBattleState* _DefensePokemon, PokemonSkill* _Skill);
 	~BattlePageSupport();
 
 
@@ -73,18 +155,17 @@ public:
 	BattlePageSupport(BattlePageSupport&& _Other) noexcept = delete;
 	BattlePageSupport& operator=(const BattlePageSupport& _Other) = delete;
 	BattlePageSupport& operator=(BattlePageSupport&& _Other) noexcept = delete;
-	TypeDamge DamgeType_;
 	int GetFinalValue()
 	{
 		return FinalValue_;
 	}
-
+	TypeDamge DamgeType_;
 
 	void UseSkillTurn();
 
 private:
-	Pokemon* AttackPokemon_;
-	Pokemon* DefensePokemon_;
+	PokemonBattleState* AttackPokemon_;
+	PokemonBattleState* DefensePokemon_;
 	PokemonSkill* AttackPokemonSkill_;
 
 	int FinalValue_;
@@ -113,10 +194,13 @@ enum class TypeDamge
 	Nothing
 };
 
-enum class PokemonState
+enum class PokemonAbility
 {
 	Att,
-	Def, 
+	Def,
+	SpAtt,
+	SpDef,
 	Speed,
-	
+	Accuracy,
+	Evasion
 };
