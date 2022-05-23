ca8a216ad4a487d93c443cccfc0d34c1e31ade18
wndgur98@gmail.com <wndgur98@gmail.com>
2022-05-23 19:43:07 +0900

    장중혁 : 오류 수정

--- Pokemon/GameEngineContents/BattleLevel.cpp
+++ Pokemon/GameEngineContents/BattleLevel.cpp
@@ -159,7 +159,6 @@ void BattleLevel::ShowEndding()
 }
 
 BattleData::BattleData()
-	:
 {
 }
 
--- Pokemon/GameEngineContents/BattleLevel.h
+++ Pokemon/GameEngineContents/BattleLevel.h
@@ -4,6 +4,7 @@
 #include "PlayerRed.h"
 #include "BattleInterface.h"
 #include "BattleEnum.h"
+#include "Pokemon.h"
 
 //선생님은 생략된 것들도 명시적으로 칠 것이다
 //직접 만들지 않아도 자동으로 생략되어 생성되 있는것들
