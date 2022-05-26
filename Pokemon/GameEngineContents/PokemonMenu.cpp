f472dab4918f54a81ec34af5427f1e9c5d669e8d
floorbear2723 <floorbear2723@gmail.com>
2022-05-26 18:34:06 +0900

    이동원 : 메뉴 업데이트

--- Pokemon/GameEngineContents/PokemonMenu.cpp
+++ Pokemon/GameEngineContents/PokemonMenu.cpp
@@ -286,7 +286,7 @@ void PokemonMenu::ChangeState(PokemonMenuType _Type)
 		SelectSwitchStart();
 		break;
 	case PokemonMenu::PokemonMenuType::Switching:
-		SwitchingStart();//
+		SwitchingStart();////
 		break;
 	default:
 		break;
